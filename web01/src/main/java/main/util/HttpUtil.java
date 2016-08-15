07fb466802925d4044c3b9156cef0e1ed26fc859
Administrator <zth390872451@gmail.com>
2016-08-16 00:59:46 +0800

    body = json格式的post请求

--- web01/src/main/java/main/util/HttpUtil.java
+++ web01/src/main/java/main/util/HttpUtil.java
@@ -1,10 +1,12 @@
 package main.util;
 
+import net.sf.json.JSONObject;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicNameValuePair;
@@ -23,7 +25,62 @@ import java.util.Map;
 public class HttpUtil {
 	
 	private static final Logger log = LoggerFactory.getLogger(HttpUtil.class);
-	
+
+	/***
+	 * 将对象序列化为JSON文本
+	 * @param object
+	 * @return
+	 */
+	public static String toJSONString(Object object)
+	{
+		JSONObject jsonObject = JSONObject.fromObject(object);
+
+		return jsonObject.toString();
+	}
+
+	/**
+	 * post请求：conten-type为json格式
+	 * @param param
+	 * @param url
+	 * @return
+	 */
+	public static String httpPostBodyRequest(Map<String,Object> param,String url){
+		CloseableHttpClient httpclient = HttpClientBuilder.create().build();
+		HttpPost httppost = new HttpPost(url);
+		String body = toJSONString(param);
+		StringEntity entity = new StringEntity(body,"utf-8");
+		try {
+			httppost.setEntity(entity);
+			HttpResponse response = httpclient.execute(httppost);
+			return EntityUtils.toString(response.getEntity());
+		} catch (Exception e) {
+			log.error("http request fail:",e);;
+			return "";
+		}
+
+	}
+
+	/**
+	 *
+	 * @param body
+	 * @param url
+     * @return
+     */
+	public static String httpPostBodyRequest(String body,String url){
+		CloseableHttpClient httpclient = HttpClientBuilder.create().build();
+		HttpPost httppost = new HttpPost(url);
+		StringEntity entity = new StringEntity(body,"utf-8");
+		try {
+			httppost.setEntity(entity);
+			HttpResponse response = httpclient.execute(httppost);
+			return EntityUtils.toString(response.getEntity());
+		} catch (Exception e) {
+			log.error("http request fail:",e);;
+			return "";
+		}
+
+	}
+
 	public static String httpPostRequest(Map<String,Object> param,String url){
 		CloseableHttpClient httpclient = HttpClientBuilder.create().build();
 		HttpPost httppost = new HttpPost(url);
